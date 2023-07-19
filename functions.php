a7c2ff7443bdded3d194f8b7572161d1a558b130
Kezeneilhou Mhasi <mkezeneilhou@gmail.com>
2023-07-19 16:45:41 +0530

    asd

--- functions.php
+++ functions.php
@@ -1,166 +1,256 @@
 <?php
-add_action( 'after_setup_theme', 'blankslate_setup' );
-function blankslate_setup() {
-load_theme_textdomain( 'blankslate', get_template_directory() . '/languages' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'responsive-embeds' );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'html5', array( 'search-form', 'navigation-widgets' ) );
-add_theme_support( 'woocommerce' );
-global $content_width;
-if ( !isset( $content_width ) ) { $content_width = 1920; }
-register_nav_menus( array( 'main-menu' => esc_html__( 'Main Menu', 'blankslate' ) ) );
+add_action('after_setup_theme', 'blankslate_setup');
+function blankslate_setup()
+{
+    load_theme_textdomain('blankslate', get_template_directory() . '/languages');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('responsive-embeds');
+    add_theme_support('automatic-feed-links');
+    add_theme_support('html5', array('search-form', 'navigation-widgets'));
+    add_theme_support('woocommerce');
+    global $content_width;
+    if (!isset($content_width)) {
+        $content_width = 1920;
+    }
+    register_nav_menus(array('main-menu' => esc_html__('Main Menu', 'blankslate')));
 }
-add_action( 'admin_notices', 'blankslate_notice' );
-function blankslate_notice() {
-$user_id = get_current_user_id();
-$admin_url = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http' ) . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$param = ( count( $_GET ) ) ? '&' : '?';
-if ( !get_user_meta( $user_id, 'blankslate_notice_dismissed_8' ) && current_user_can( 'manage_options' ) )
-echo '<div class="notice notice-info"><p><a href="' . esc_url( $admin_url ), esc_html( $param ) . 'dismiss" class="alignright" style="text-decoration:none"><big>' . esc_html__( 'Ⓧ', 'blankslate' ) . '</big></a>' . wp_kses_post( __( '<big><strong>📝 Thank you for using BlankSlate!</strong></big>', 'blankslate' ) ) . '<br /><br /><a href="https://wordpress.org/support/theme/blankslate/reviews/#new-post" class="button-primary" target="_blank">' . esc_html__( 'Review', 'blankslate' ) . '</a> <a href="https://github.com/tidythemes/blankslate/issues" class="button-primary" target="_blank">' . esc_html__( 'Feature Requests & Support', 'blankslate' ) . '</a> <a href="https://calmestghost.com/donate" class="button-primary" target="_blank">' . esc_html__( 'Donate', 'blankslate' ) . '</a></p></div>';
+add_action('admin_notices', 'blankslate_notice');
+function blankslate_notice()
+{
+    $user_id = get_current_user_id();
+    $admin_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $param = (count($_GET)) ? '&' : '?';
+    if (!get_user_meta($user_id, 'blankslate_notice_dismissed_8') && current_user_can('manage_options'))
+        echo '<div class="notice notice-info"><p><a href="' . esc_url($admin_url), esc_html($param) . 'dismiss" class="alignright" style="text-decoration:none"><big>' . esc_html__('Ⓧ', 'blankslate') . '</big></a>' . wp_kses_post(__('<big><strong>📝 Thank you for using BlankSlate!</strong></big>', 'blankslate')) . '<br /><br /><a href="https://wordpress.org/support/theme/blankslate/reviews/#new-post" class="button-primary" target="_blank">' . esc_html__('Review', 'blankslate') . '</a> <a href="https://github.com/tidythemes/blankslate/issues" class="button-primary" target="_blank">' . esc_html__('Feature Requests & Support', 'blankslate') . '</a> <a href="https://calmestghost.com/donate" class="button-primary" target="_blank">' . esc_html__('Donate', 'blankslate') . '</a></p></div>';
 }
-add_action( 'admin_init', 'blankslate_notice_dismissed' );
-function blankslate_notice_dismissed() {
-$user_id = get_current_user_id();
-if ( isset( $_GET['dismiss'] ) )
-add_user_meta( $user_id, 'blankslate_notice_dismissed_8', 'true', true );
+add_action('admin_init', 'blankslate_notice_dismissed');
+function blankslate_notice_dismissed()
+{
+    $user_id = get_current_user_id();
+    if (isset($_GET['dismiss']))
+        add_user_meta($user_id, 'blankslate_notice_dismissed_8', 'true', true);
 }
-add_action( 'wp_enqueue_scripts', 'blankslate_enqueue' );
-function blankslate_enqueue() {
-wp_enqueue_style( 'blankslate-style', get_stylesheet_uri() );
-wp_enqueue_script( 'jquery' );
+add_action('wp_enqueue_scripts', 'blankslate_enqueue');
+function blankslate_enqueue()
+{
+    wp_enqueue_style('blankslate-style', get_stylesheet_uri());
+    wp_enqueue_script('jquery');
 }
-add_action( 'wp_footer', 'blankslate_footer' );
-function blankslate_footer() {
+add_action('wp_footer', 'blankslate_footer');
+function blankslate_footer()
+{
 ?>
 <script>
-jQuery(document).ready(function($) {
-var deviceAgent = navigator.userAgent.toLowerCase();
-if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
-$("html").addClass("ios");
-$("html").addClass("mobile");
-}
-if (deviceAgent.match(/(Android)/)) {
-$("html").addClass("android");
-$("html").addClass("mobile");
-}
-if (navigator.userAgent.search("MSIE") >= 0) {
-$("html").addClass("ie");
-}
-else if (navigator.userAgent.search("Chrome") >= 0) {
-$("html").addClass("chrome");
-}
-else if (navigator.userAgent.search("Firefox") >= 0) {
-$("html").addClass("firefox");
-}
-else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-$("html").addClass("safari");
-}
-else if (navigator.userAgent.search("Opera") >= 0) {
-$("html").addClass("opera");
-}
-});
+    jQuery(document).ready(function ($) {
+        var deviceAgent = navigator.userAgent.toLowerCase();
+        if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
+            $("html").addClass("ios");
+            $("html").addClass("mobile");
+        }
+        if (deviceAgent.match(/(Android)/)) {
+            $("html").addClass("android");
+            $("html").addClass("mobile");
+        }
+        if (navigator.userAgent.search("MSIE") >= 0) {
+            $("html").addClass("ie");
+        } else if (navigator.userAgent.search("Chrome") >= 0) {
+            $("html").addClass("chrome");
+        } else if (navigator.userAgent.search("Firefox") >= 0) {
+            $("html").addClass("firefox");
+        } else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+            $("html").addClass("safari");
+        } else if (navigator.userAgent.search("Opera") >= 0) {
+            $("html").addClass("opera");
+        }
+    });
 </script>
 <?php
 }
-add_filter( 'document_title_separator', 'blankslate_document_title_separator' );
-function blankslate_document_title_separator( $sep ) {
-$sep = esc_html( '|' );
-return $sep;
+add_filter('document_title_separator', 'blankslate_document_title_separator');
+function blankslate_document_title_separator($sep)
+{
+    $sep = esc_html('|');
+    return $sep;
 }
-add_filter( 'the_title', 'blankslate_title' );
-function blankslate_title( $title ) {
-if ( $title == '' ) {
-return esc_html( '...' );
-} else {
-return wp_kses_post( $title );
+add_filter('the_title', 'blankslate_title');
+function blankslate_title($title)
+{
+    if ($title == '') {
+        return esc_html('...');
+    } else {
+        return wp_kses_post($title);
+    }
 }
+function blankslate_schema_type()
+{
+    $schema = 'https://schema.org/';
+    if (is_single()) {
+        $type = "Article";
+    } elseif (is_author()) {
+        $type = 'ProfilePage';
+    } elseif (is_search()) {
+        $type = 'SearchResultsPage';
+    } else {
+        $type = 'WebPage';
+    }
+    echo 'itemscope itemtype="' . esc_url($schema) . esc_attr($type) . '"';
 }
-function blankslate_schema_type() {
-$schema = 'https://schema.org/';
-if ( is_single() ) {
-$type = "Article";
-} elseif ( is_author() ) {
-$type = 'ProfilePage';
-} elseif ( is_search() ) {
-$type = 'SearchResultsPage';
-} else {
-$type = 'WebPage';
+add_filter('nav_menu_link_attributes', 'blankslate_schema_url', 10);
+function blankslate_schema_url($atts)
+{
+    $atts['itemprop'] = 'url';
+    return $atts;
 }
-echo 'itemscope itemtype="' . esc_url( $schema ) . esc_attr( $type ) . '"';
+if (!function_exists('blankslate_wp_body_open')) {
+    function blankslate_wp_body_open()
+    {
+        do_action('wp_body_open');
+    }
 }
-add_filter( 'nav_menu_link_attributes', 'blankslate_schema_url', 10 );
-function blankslate_schema_url( $atts ) {
-$atts['itemprop'] = 'url';
-return $atts;
+add_action('wp_body_open', 'blankslate_skip_link', 5);
+function blankslate_skip_link()
+{
+    echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__('Skip to the content', 'blankslate') . '</a>';
 }
-if ( !function_exists( 'blankslate_wp_body_open' ) ) {
-function blankslate_wp_body_open() {
-do_action( 'wp_body_open' );
+add_filter('the_content_more_link', 'blankslate_read_more_link');
+function blankslate_read_more_link()
+{
+    if (!is_admin()) {
+        return ' <a href="' . esc_url(get_permalink()) . '" class="more-link">' . sprintf(__('...%s', 'blankslate'), '<span class="screen-reader-text">  ' . esc_html(get_the_title()) . '</span>') . '</a>';
+    }
 }
+add_filter('excerpt_more', 'blankslate_excerpt_read_more_link');
+function blankslate_excerpt_read_more_link($more)
+{
+    if (!is_admin()) {
+        global $post;
+        return ' <a href="' . esc_url(get_permalink($post->ID)) . '" class="more-link">' . sprintf(__('...%s', 'blankslate'), '<span class="screen-reader-text">  ' . esc_html(get_the_title()) . '</span>') . '</a>';
+    }
 }
-add_action( 'wp_body_open', 'blankslate_skip_link', 5 );
-function blankslate_skip_link() {
-echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__( 'Skip to the content', 'blankslate' ) . '</a>';
+add_filter('big_image_size_threshold', '__return_false');
+add_filter('intermediate_image_sizes_advanced', 'blankslate_image_insert_override');
+function blankslate_image_insert_override($sizes)
+{
+    unset($sizes['medium_large']);
+    unset($sizes['1536x1536']);
+    unset($sizes['2048x2048']);
+    return $sizes;
 }
-add_filter( 'the_content_more_link', 'blankslate_read_more_link' );
-function blankslate_read_more_link() {
-if ( !is_admin() ) {
-return ' <a href="' . esc_url( get_permalink() ) . '" class="more-link">' . sprintf( __( '...%s', 'blankslate' ), '<span class="screen-reader-text">  ' . esc_html( get_the_title() ) . '</span>' ) . '</a>';
+add_action('widgets_init', 'blankslate_widgets_init');
+function blankslate_widgets_init()
+{
+    register_sidebar(array(
+        'name' => esc_html__('Sidebar Widget Area', 'blankslate'),
+        'id' => 'primary-widget-area',
+        'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</li>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
 }
+add_action('wp_head', 'blankslate_pingback_header');
+function blankslate_pingback_header()
+{
+    if (is_singular() && pings_open()) {
+        printf('<link rel="pingback" href="%s" />' . "\n", esc_url(get_bloginfo('pingback_url')));
+    }
 }
-add_filter( 'excerpt_more', 'blankslate_excerpt_read_more_link' );
-function blankslate_excerpt_read_more_link( $more ) {
-if ( !is_admin() ) {
-global $post;
-return ' <a href="' . esc_url( get_permalink( $post->ID ) ) . '" class="more-link">' . sprintf( __( '...%s', 'blankslate' ), '<span class="screen-reader-text">  ' . esc_html( get_the_title() ) . '</span>' ) . '</a>';
+add_action('comment_form_before', 'blankslate_enqueue_comment_reply_script');
+function blankslate_enqueue_comment_reply_script()
+{
+    if (get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-}
-add_filter( 'big_image_size_threshold', '__return_false' );
-add_filter( 'intermediate_image_sizes_advanced', 'blankslate_image_insert_override' );
-function blankslate_image_insert_override( $sizes ) {
-unset( $sizes['medium_large'] );
-unset( $sizes['1536x1536'] );
-unset( $sizes['2048x2048'] );
-return $sizes;
-}
-add_action( 'widgets_init', 'blankslate_widgets_init' );
-function blankslate_widgets_init() {
-register_sidebar( array(
-'name' => esc_html__( 'Sidebar Widget Area', 'blankslate' ),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => '</li>',
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-}
-add_action( 'wp_head', 'blankslate_pingback_header' );
-function blankslate_pingback_header() {
-if ( is_singular() && pings_open() ) {
-printf( '<link rel="pingback" href="%s" />' . "\n", esc_url( get_bloginfo( 'pingback_url' ) ) );
-}
-}
-add_action( 'comment_form_before', 'blankslate_enqueue_comment_reply_script' );
-function blankslate_enqueue_comment_reply_script() {
-if ( get_option( 'thread_comments' ) ) {
-wp_enqueue_script( 'comment-reply' );
-}
-}
-function blankslate_custom_pings( $comment ) {
+function blankslate_custom_pings($comment)
+{
 ?>
-<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo esc_url( comment_author_link() ); ?></li>
+<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo esc_url(comment_author_link()); ?>
+</li>
 <?php
 }
-add_filter( 'get_comments_number', 'blankslate_comment_count', 0 );
-function blankslate_comment_count( $count ) {
-if ( !is_admin() ) {
-global $id;
-$get_comments = get_comments( 'status=approve&post_id=' . $id );
-$comments_by_type = separate_comments( $get_comments );
-return count( $comments_by_type['comment'] );
-} else {
-return $count;
+add_filter('get_comments_number', 'blankslate_comment_count', 0);
+function blankslate_comment_count($count)
+{
+    if (!is_admin()) {
+        global $id;
+        $get_comments = get_comments('status=approve&post_id=' . $id);
+        $comments_by_type = separate_comments($get_comments);
+        return count($comments_by_type['comment']);
+    } else {
+        return $count;
+    }
 }
+function register_my_menu() {
+    register_nav_menu('header-menu',__( 'Header Menu' ));
+  }
+  add_action( 'init', 'register_my_menu' );
+// create a walker for wordpress menu
+/**
+ * Create HTML list of nav menu items.
+ * Replacement for the native Walker, using the description.
+ *
+ * @see    http://wordpress.stackexchange.com/q/14037/
+ * @author toscho, http://toscho.de
+ */
+add_filter( 'nav_menu_link_attributes', 'nav_menu_link_class', 10, 2 );
+function nav_menu_link_class( $atts, $item ) {
+    if(  $item->has_children ) {
+        $class         = 'has-ripple';
+        $atts['class'] = $class;
+    }
+
+    return $atts;
+}
+class Blank_Slate_Walker extends Walker_Nav_Menu {
+    /**
+     * Start the element output.
+     *
+     * @param  string $output Passed by reference. Used to append additional content.
+     * @param  object $item   Menu item data object.
+     * @param  int $depth     Depth of menu item. May be used for padding.
+     * @param  array $args    Additional strings.
+     * @return void
+     */
+    // apply class if parent if menu has children
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        // add classes to li
+        $classes     = empty ( $item->classes ) ? array () : (array) $item->classes;
+        $class_names = join( ' dropdown ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
+        // $class_names = ' class="menu-item menu-item-' . $item->ID . '"';
+        $class_names = ' class="menu-item menu-item-' . $item->ID . ' ' . esc_attr( $class_names ) . '"';
+        // add li to output
+        $output .= "<li $class_names>";
+        // add anchor to output
+        $attributes  = '';
+        ! empty ( $item->attr_title )
+        and $attributes .= ' title="'  . esc_attr( $item->attr_title ) .'"';
+        ! empty ( $item->target )
+        and $attributes .= ' target="' . esc_attr( $item->target     ) .'"';
+        ! empty ( $item->xfn )
+        and $attributes .= ' rel="'    . esc_attr( $item->xfn        ) .'"';
+        ! empty ( $item->url )
+        and $attributes .= ' href="'   . esc_attr( $item->url        ) .'"';
+        $attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+        $title = apply_filters( 'the_title', $item->title, $item->ID );
+        // $title = apply_filters( 'nav_menu_item_title', $item->title, $item, $args, $depth );
+        if($this->has_children)
+        {
+            $item_output = "<a $attributes>$title <i class='bi bi-chevron-down'></i></a>";
+        }else{
+            $item_output = "<a $attributes>$title</a>";
+
+        }
+        // add anchor to output
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+
+    function end_lvl(&$output, $depth=0, $args=null) {
+        if($depth == 1)
+        {
+            $item_output = "</ul>\n<i class='bi bi-list mobile-nav-toggle'></i>";
+            $output .= apply_filters( 'walker_nav_menu_end_lvl', $item_output, $depth, $args );
+        }
+    } 
 }

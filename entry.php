a421e10d50de9dc7dc26f01b150e96dd0338e2cb
Kezeneilhou Mhasi <mkezeneilhou@gmail.com>
2023-07-20 15:04:49 +0530

    asd

--- entry.php
+++ entry.php
@@ -1,12 +1,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header>
-        <?php if ( is_singular() ) { echo '<h1 class="entry-title mb-0" itemprop="headline">'; } else { echo '<h3 class="entry-title mb-0">'; } ?>
+        <?php if ( is_singular() ) { echo ''; } else { echo '<h3 class="entry-title mb-0">'; } ?>
         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"
             class="mb-0"><?php the_title(); ?></a>
-        <?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h3>'; } ?>
+        <?php if ( is_singular() ) { echo ''; } else { echo '</h3>'; } ?>
         <?php edit_post_link(); ?>
         <?php if ( !is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
     </header>
     <?php get_template_part( 'entry', ( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
-    <?php if ( is_singular() ) { get_template_part( 'entry-footer' ); } ?>
+    <!-- <?php if ( is_singular() ) { get_template_part( 'entry-footer' ); } ?> -->
 </article>
--- single.php
+++ single.php
@@ -1,20 +1,26 @@
 <?php get_header(); ?>
-<section class="main">
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<section id="breadcrumbs" class="breadcrumbs">
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+            <h2><?php the_title(); ?></h2>
+            <ol>
+                <li><a href="<?php $site_url = home_url(); echo $site_url;?>">Home</a></li>
+                <li><?php the_title(); ?></li>
+            </ol>
+        </div>
+
+    </div>
+</section>
+<section>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'entry' ); ?>
-                <?php if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
+                <?php the_content(); ?>
                 <?php endwhile; endif; ?>
             </div>
         </div>
     </div>
-
-
 </section>
-
-<footer class="footer">
-    <?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
 <?php get_footer(); ?>
